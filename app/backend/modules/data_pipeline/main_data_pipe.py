9f9e8b9d509687983f02fee16caf1758089cc520
jesvijonathan <jesvi22j@gmail.com>
2025-09-15 02:21:55 +0530

    template

---
+++ app/backend/modules/data_pipeline/main_data_pipe.py
@@ -0,0 +1,11 @@
+twitter
+news scraper
+reddit scraper
+stock api
+
+M8ZKYDAIEVC2JNAE
+https://www.alphavantage.co/
+https://www.bavest.co/de
+profit analysis statement - web crawler
+reviews - web crawler
+customer feedback - web crawler
