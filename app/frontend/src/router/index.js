4ba301b04b2b149d89124b43494fe11f0ad7a164
Atharva Kutwal <atharvakutwal2002@gmail.com>
2025-09-18 16:51:04 +0530

    router added

--- app/frontend/src/router/index.js
+++ app/frontend/src/router/index.js
@@ -12,8 +12,13 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
+      path: '/',
       name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/login',
+      name: 'login',
       component: HomeView,
     },
     {
